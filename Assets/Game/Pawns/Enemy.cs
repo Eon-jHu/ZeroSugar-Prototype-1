a43aab741091ed19e6a9e9e0bdcfbb316e9b6397
DogsAreHumansToo <marc_laban@hotmail.com>
2025-02-24 09:56:36 +1300

    Enemy Spawn Works

--- Assets/Game/Pawns/Enemy.cs
+++ Assets/Game/Pawns/Enemy.cs
@@ -22,22 +22,34 @@ public class Enemy : MonoBehaviour, IOccupier
     // Start is called before the first frame update
     void Start()
     {
-        
+        Board.OnBoardReady += EnemySpawn;
     }
 
     private void EnemySpawn(Board board)
     {
         List<Tile> tiles = board.GetTiles();
+        List<Tile> spawnableTiles = new();
 
-        //foreach (Tile tileInstance in tiles)
-        //{
-        //    if (tileInstance.IsBoundaryTile == true)
-        //    {
+        foreach (Tile tileInstance in tiles)
+        {
+            if (tileInstance.IsBoundaryTile() == true && tileInstance.IsOccupied() == false)
+            {
+                spawnableTiles.Add(tileInstance);
+                
+                
 
-        //    }
-        //}
-        
+            }
+        }
+        Tile randomTile = spawnableTiles[Random.Range(0, spawnableTiles.Count)];
+        PlaceEnemy(randomTile);
+
     }
+
+    private void PlaceEnemy(Tile tile)
+    {
+        tile.OccupyTile(this, null, true);
+    }
+
     // Update is called once per frame
     void Update()
     {
