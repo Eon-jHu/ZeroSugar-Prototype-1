7ade23e146884c52b02d039bd080379473feb879
DogsAreHumansToo <marc_laban@hotmail.com>
2025-03-02 17:51:55 +1300

    Final fix to Main Branch

---
+++ Assets/Game/GameOverCheck.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverCheck : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject gameOverCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("GameOverManager script is running!");
+        gameOverCanvas = GameObject.Find("GameOverCanvas");
+        gameOverCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Player.Instance.PlayerAliveCheck() == false)
+        {
+            gameOverCanvas.SetActive(true);
+        }
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("GameTest");
+    }
+}
